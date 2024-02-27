ae1492b9841923359d0bd233d8dfc41a51012183
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2024-02-28 00:48:25 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Hassan Mohammed on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
